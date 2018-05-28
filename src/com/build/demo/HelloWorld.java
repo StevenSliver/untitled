9bff5d306c2ba4279c2f6c44c7bcda1464198f51
史超 <shichao@nantian.com.cn>
2018-05-28 15:14:44 +0800

    Initial commit

---
+++ src/com/build/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.build.demo;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!!!");
+        String test = null;
+    }
+
+}
---
+++ src/com/build/demo/test.java
@@ -0,0 +1,7 @@
+package com.build.demo;
+
+/**
+ * Created by scpor on 2018/5/28.
+ */
+public class test {
+}
